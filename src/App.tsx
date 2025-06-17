c07d9a2ef1559aa17739895801960d4889fe7685
xcedou1 <cedric-beaulieu@outlook.com>
2025-06-17 14:18:57 -0400

    ads
    sadasd

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import PriceChart from './components/PriceChart';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Trading Academy</h1>
+      <PriceChart />
+    </div>
+  );
+}
+
+export default App;
